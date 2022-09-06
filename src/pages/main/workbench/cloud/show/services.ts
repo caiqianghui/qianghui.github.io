094d1efaba63ee0a0adde32db2b21c274065dc60
caiqianghui@netfarmer.com.cn <caiqianghui@netfarmer.com.cn>
2022-09-06 15:50:51 +0800

    1

--- src/pages/main/workbench/cloud/show/services.ts
+++
@@ -1,7 +0,0 @@
-import HttpClient from "src/utils/http/cloud/HttpClient";
-
-export async function getModelProcesses(params: any) {
-  return HttpClient.get('/api/v1/system_process/model_processes', {
-    params,
-  });
-}
--- src/pages/main/workbench/cloud/signin/index.config.ts
+++
@@ -1,3 +0,0 @@
-export default {
-  navigationBarTitleText: '签到打卡'
-}
